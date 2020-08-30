a2913f12ae1835586ab844e3d7bbd31297666492
apklx <pchs@ya.ru>
2020-08-30 12:14:28 +0300

    3.3. Разбор первой программы. [#350333]

---
+++ src/ru/job4j/Multiple.java
@@ -0,0 +1,14 @@
+package ru.job4j;
+
+public class Multiple {
+    public static void main(String[] args){
+        System.out.print("1 * 2 = 2\n");
+        System.out.print("1 * 3 = 3\n");
+        System.out.print("1 * 4 = 4\n");
+        System.out.print("1 * 5 = 5\n");
+        System.out.print("1 * 6 = 6\n");
+        System.out.print("1 * 7 = 7\n");
+        System.out.print("1 * 8 = 8\n");
+        System.out.print("1 * 9 = 9");
+    }
+}
